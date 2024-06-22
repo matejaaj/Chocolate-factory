d6d77c175a1d3174983a0f67916113ad0b525de6
Bogdan Djukic <bogdandjukic25@gmail.com>
2024-06-22 20:11:46 +0200

    ispravke sitnih gresaka

--- frontend/src/views/FactoryDetails.vue
+++ frontend/src/views/FactoryDetails.vue
@@ -10,9 +10,8 @@
 		<div>
 			<button v-if="isManager" @click="viewFactoryOrders">View Orders</button>
 			<button @click="viewFactoryComments">Show Comments</button>
-			<button @click="viewRegisterEmployee">Register employee</button>
+			<button v-if="isManager" @click="viewRegisterEmployee">Register employee</button>
 		</div>
-		<chocolate-list :factoryId="factory.id" />
 	</div>
 	<div v-else>
 		<p>Loading factory details...</p>
